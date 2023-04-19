968595b9571d96af44b7138121e4abe48a4d60f0
Rishav Mukherjee <rishavmukherjee11@gmail.com>
2023-04-19 10:56:56 +0530

    try

--- app.config.js
+++ app.config.js
@@ -27,10 +27,15 @@ export default {
       projectId: process.env.PROJECT_ID,
       storageBucket: process.env.STORAGE_BUCKET,
       messagingSenderId: process.env.MESSAGING_SENDER_ID,
-      appId: process.env.APP_ID
+      appId: process.env.APP_ID,
+      eas: {
+        projectId: "7a91c17d-529e-4433-be41-3a616475e31c"
+      }
     },
     android: {
-      permissions: ['NOTIFICATIONS']
+      permissions: ['NOTIFICATIONS'],
+      package: "com.travell.trackmate"
     }
+    
   }
 };
